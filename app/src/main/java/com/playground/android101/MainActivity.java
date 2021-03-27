667f833ce830618374388fc06b1906efdcd62d1a
Køπs†åπ†iπ <Konstantin.Vankov.Extern@fiduciagad.de>
2021-03-27 16:51:24 +0100

    simple alert dialog

--- app/src/main/java/com/playground/android101/MainActivity.java
+++ app/src/main/java/com/playground/android101/MainActivity.java
@@ -1,8 +1,10 @@
 package com.playground.android101;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn).setOnClickListener((v -> showMyDialog()));
+    }
+
+    private void showMyDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("My title")
+                .setMessage("My long and unique message...")
+                .setPositiveButton("Positive", (dialog, which) -> Toast.makeText(this, "Positive button was clicked", Toast.LENGTH_LONG).show())
+                .setNegativeButton("Negative", (dialog, which) -> Toast.makeText(this, "Negative button was clicked", Toast.LENGTH_LONG).show())
+                .setCancelable(false)
+                .create()
+                .show();
     }
 }
